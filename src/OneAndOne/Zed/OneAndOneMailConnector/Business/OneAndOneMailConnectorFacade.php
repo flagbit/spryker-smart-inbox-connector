6ba9480796008d40e6fc4ba3589e3d94c974168d
Michael Vogel <michael.vogel@flagbit.de>
2021-01-23 18:58:06 +0100

    [license] Add facade

---
+++ src/OneAndOne/Zed/OneAndOneMailConnector/Business/OneAndOneMailConnectorFacade.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OneAndOne\Zed\OneAndOneMailConnector\Business;
+
+use Generated\Shared\Transfer\MailTransfer;
+use Generated\Shared\Transfer\OrderTransfer;
+use Spryker\Zed\Kernel\Business\AbstractFacade;
+
+class OneAndOneMailConnectorFacade extends AbstractFacade
+    implements OneAndOneMailConnectorFacadeInterface
+{
+    public function expendOrderMailTransfer(MailTransfer $mailTransfer, OrderTransfer $orderTransfer): MailTransfer
+    {
+        return $this->getFactory()
+            ->createSchemaOrgOrderMailExpander()
+            ->expandOrderMailTransfer($mailTransfer, $orderTransfer);
+    }
+}
