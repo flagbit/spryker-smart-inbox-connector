e2c493b87b01c3ffd536576499708d0d11760fe9
Michael Vogel <michael.vogel@flagbit.de>
2021-01-24 13:40:44 +0100

    [license] Move plugin class to correct location

--- src/OneAndOne/Zed/OneAndOneMailConnector/Communication/Plugin/OneAndOneMailConnectorOrderMailExpanderPlugin.php
+++ src/OneAndOne/Zed/OneAndOneMailConnector/Communication/Plugin/OneAndOneMailConnectorOrderMailExpanderPlugin.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OneAndOne\Zed\OneAndOneMailConnector\Dependency\Plugin;
+namespace OneAndOne\Zed\OneAndOneMailConnector\Communication\Plugin;
 
 use Generated\Shared\Transfer\MailTransfer;
 use Generated\Shared\Transfer\OrderTransfer;
@@ -9,6 +9,14 @@ use Spryker\Zed\OmsExtension\Dependency\Plugin\OmsOrderMailExpanderPluginInterfa
 
 class OneAndOneMailConnectorOrderMailExpanderPlugin extends AbstractPlugin implements OmsOrderMailExpanderPluginInterface
 {
+    /**
+     * @method \Spryker\Zed\OneAndOneMailConnector\Business\OneAndOneMailConnectorFacade getFacade()
+     *
+     * @param MailTransfer  $mailTransfer
+     * @param OrderTransfer $orderTransfer
+     *
+     * @return MailTransfer
+     */
     public function expand(MailTransfer $mailTransfer, OrderTransfer $orderTransfer): MailTransfer
     {
         return $this->getFacade()->expendOrderMailTransfer($mailTransfer, $orderTransfer);
