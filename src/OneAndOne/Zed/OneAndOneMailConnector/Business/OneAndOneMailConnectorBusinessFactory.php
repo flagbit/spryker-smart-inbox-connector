7f5c9662b7ce90b4277e5933542e730f46200478
Michael Vogel <michael.vogel@flagbit.de>
2021-02-02 14:37:57 +0100

    [implementation] Remove unused creates from factory

--- src/OneAndOne/Zed/OneAndOneMailConnector/Business/OneAndOneMailConnectorBusinessFactory.php
+++ src/OneAndOne/Zed/OneAndOneMailConnector/Business/OneAndOneMailConnectorBusinessFactory.php
@@ -2,10 +2,6 @@
 
 namespace OneAndOne\Zed\OneAndOneMailConnector\Business;
 
-use EinsUndEins\SchemaOrgMailBody\Model\Order;
-use EinsUndEins\SchemaOrgMailBody\Model\ParcelDelivery;
-use EinsUndEins\SchemaOrgMailBody\Renderer\OrderRenderer;
-use EinsUndEins\SchemaOrgMailBody\Renderer\ParcelDeliveryRenderer;
 use Generated\Shared\Transfer\MailTemplateTransfer;
 use Generated\Shared\Transfer\OneAndOneMailConnectorTransfer;
 use Generated\Shared\Transfer\SchemaOrgData;
@@ -15,50 +11,6 @@ use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
 
 class OneAndOneMailConnectorBusinessFactory extends AbstractBusinessFactory
 {
-    /**
-     * @param OneAndOneMailConnectorTransfer $transfer
-     *
-     * @return Order
-     */
-    public function createOrder(OneAndOneMailConnectorTransfer $transfer): Order
-    {
-        return new Order($transfer->getOrderNumber(), $transfer->getOrderStatus(), $transfer->getShopName());
-    }
-
-    /**
-     * @param OneAndOneMailConnectorTransfer $transfer
-     *
-     * @return ParcelDelivery
-     */
-    public function createParcelDelivery(OneAndOneMailConnectorTransfer $transfer): ParcelDelivery {
-        return new ParcelDelivery(
-            $transfer->getDeliveryName(),
-            $transfer->getTrackingNumber(),
-            $transfer->getOrderNumber(),
-            $transfer->getOrderStatus(),
-            $transfer->getShopName());
-    }
-
-    /**
-     * @param Order $order
-     *
-     * @return OrderRenderer
-     */
-    public function createOrderRenderer(Order $order): OrderRenderer
-    {
-        return new OrderRenderer($order);
-    }
-
-    /**
-     * @param ParcelDelivery $parcelDelivery
-     *
-     * @return ParcelDeliveryRenderer
-     */
-    public function createParcelDeliveryRenderer(ParcelDelivery $parcelDelivery): ParcelDeliveryRenderer
-    {
-        return new ParcelDeliveryRenderer($parcelDelivery);
-    }
-
     /**
      * @return SchemaOrgOrderMailExpanderInterface
      */
