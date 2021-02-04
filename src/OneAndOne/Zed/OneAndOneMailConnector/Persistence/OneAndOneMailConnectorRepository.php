da5a8432ca1b1c8b43f2fa30c5ca2d6028b19225
Michael Vogel <michael.vogel@flagbit.de>
2021-02-04 17:45:53 +0100

    [implementation] Remove unused repo method

--- src/OneAndOne/Zed/OneAndOneMailConnector/Persistence/OneAndOneMailConnectorRepository.php
+++ src/OneAndOne/Zed/OneAndOneMailConnector/Persistence/OneAndOneMailConnectorRepository.php
@@ -7,21 +7,6 @@ use Spryker\Zed\Kernel\Persistence\AbstractRepository;
 
 class OneAndOneMailConnectorRepository extends AbstractRepository implements OneAndOneMailConnectorRepositoryInterface
 {
-    public function findSpySalesOrderItemById(int $idOrderItem)
-    {
-        $orderItemEntity = $this
-            ->getFactory()
-            ->createSpySalesOrderItemQuery()
-            ->filterByIdSalesOrderItem($idOrderItem)
-            ->findOne();
-
-        if (!$orderItemEntity) {
-            return null;
-        }
-
-        return null; // @TODO return value
-    }
-
     /**
      * @param array $idOrderItems
      *
