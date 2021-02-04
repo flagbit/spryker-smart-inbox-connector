e0fdcf6722eb6c6e5c0d8b674e81dc93d7765d06
Michael Vogel <michael.vogel@flagbit.de>
2021-02-04 17:45:53 +0100

    [implementation] remove unused files

--- src/OneAndOne/Zed/OneAndOneMailConnector/Persistence/OneAndOneMailConnectorRepositoryInterface.php
+++ src/OneAndOne/Zed/OneAndOneMailConnector/Persistence/OneAndOneMailConnectorRepositoryInterface.php
@@ -4,7 +4,5 @@ namespace OneAndOne\Zed\OneAndOneMailConnector\Persistence;
 
 interface OneAndOneMailConnectorRepositoryInterface
 {
-    public function findSpySalesOrderItemById(int $idOrderItem);
-
     public function findSpySalesOrderItemsById(array $idOrderItems);
 }
