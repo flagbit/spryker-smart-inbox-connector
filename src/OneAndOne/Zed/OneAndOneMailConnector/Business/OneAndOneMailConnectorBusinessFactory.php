0204bbc3dc86b4f7d9d1183e8ea973d4bfffe79a
Michael Vogel <michael.vogel@flagbit.de>
2021-02-05 16:18:01 +0100

    [implementation] Add ParcelDeliveryFactory to BusinessFactory

--- src/OneAndOne/Zed/OneAndOneMailConnector/Business/OneAndOneMailConnectorBusinessFactory.php
+++ src/OneAndOne/Zed/OneAndOneMailConnector/Business/OneAndOneMailConnectorBusinessFactory.php
@@ -5,6 +5,7 @@ namespace OneAndOne\Zed\OneAndOneMailConnector\Business;
 use Generated\Shared\Transfer\MailTemplateTransfer;
 use Generated\Shared\Transfer\OneAndOneMailConnectorTransfer;
 use Generated\Shared\Transfer\SchemaOrgTransfer;
+use OneAndOne\Zed\OneAndOneMailConnector\Business\ParcelDelivery\ParcelDeliveryFactory;
 use OneAndOne\Zed\OneAndOneMailConnector\Business\SchemaOrgOrderMailExpander\SchemaOrgOrderMailExpanderInterface;
 use OneAndOne\Zed\OneAndOneMailConnector\Business\SchemaOrgOrderMailExpander\SchemaOrgOrderMailExpander;
 use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
@@ -33,5 +34,13 @@ class OneAndOneMailConnectorBusinessFactory extends AbstractBusinessFactory
     public function createSchemaOrgTransfer(): SchemaOrgTransfer
     {
         return new SchemaOrgTransfer();
+    }
+
+    /**
+     * @return ParcelDeliveryFactory
+     */
+    protected function createParcelDeliveryFactory(): ParcelDeliveryFactory
+    {
+        return new ParcelDeliveryFactory();
     }
 }
