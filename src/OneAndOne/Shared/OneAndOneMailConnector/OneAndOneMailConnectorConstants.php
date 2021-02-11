fb80fc03aba445b9bc36e92d720381fea10c3b49
Michael Vogel <michael.vogel@flagbit.de>
2021-02-11 16:56:33 +0100

    [implementation] Fix constants

--- src/OneAndOne/Shared/OneAndOneMailConnector/OneAndOneMailConnectorConstants.php
+++ src/OneAndOne/Shared/OneAndOneMailConnector/OneAndOneMailConnectorConstants.php
@@ -4,6 +4,6 @@ namespace OneAndOne\Shared\OneAndOneMailConnector;
 
 interface OneAndOneMailConnectorConstants
 {
-    public const MATRIX_KEY = 'ONE_AND_ONE_MAIL_CONNECTOR:MATRIX_KEY';
-    public const SHOP_NAME = 'ONE_AND_ONE_MAIL_CONNECTOR:SHOP_NAME';
+    public const MATRIX_KEY = 'ONE_AND_ONE_MAIL_CONNECTOR_MATRIX_KEY';
+    public const SHOP_NAME = 'ONE_AND_ONE_MAIL_CONNECTOR_SHOP_NAME';
 }
